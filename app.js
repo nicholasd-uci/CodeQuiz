1b457d50cd47a09acef8cd75f2b369eca9fc21cc
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-07-28 00:25:47 -0700

    createdElement btn for answers

--- app.js
+++ app.js
@@ -33,12 +33,25 @@ let questions = [
 ]
 
 // this is the variable for our questions index//
-let questionIndex = 0
+let Index = 0
 
 
 // takes the current index that we are at, and then should go grab the questions information for us to render//
 const newQuestion = () => {
-  document.getElementById(`question`).textContent = questions[questionIndex].question
+
+  // this is using the ID call to ??questions in array?? by using the .textContent// then setting it equal to questions array and giving it a LET of Index = 0. Then using .Notation to call for which array you want to pull from.//
+  document.getElementById(`question`).textContent = questions[Index].question
+
+  // this is calling for a LET var of answers to be calling from the questions array? using .Notation//
+  let answers = questions[Index].answers
+
+  // this is using the LET answers to be called on. the innerHTML sets or returns the HTML content of an Element** // for loop  let us start at 0 in the index and move up one at time and calling each section of **answers inside of Questions ARRAY!//
+  document.getElementById('answers').innerHTML = ''
+  for (let i = 0; i < answers.length; i++) {
+    // this is an createElement METHOD! this lets you create clickable answer buttons//
+    let answerElem = document.createElement('button')
+
+  }
 }
 
 
