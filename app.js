5d74e8ce96936af0997a102c9cee64bc94353bce
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-07-27 23:45:31 -0700

    updated .js with let questions

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-
-<body>
-
+let questions = [
+  {
+    "question": "What document do you use to style an index.HTML file? ",
+    "correct_answer": ".css file",
+    "answers": [
+      ".style file",
+      "style.js file",
+      ".css file",
+      ".js file"
+    ]
+  },
+  {
+    "question": "Arrays in JavaScript can be used to store _____.",
+    "correct_answer": "all of the above",
+    "answers": [
+      "Ints & Strings",
+      "another array",
+      "booleans",
+      "all of the above"
+    ]
+  },
+  {
+  "question": "What are booleans?",
+  "correct_answer": "True or False",
+  "answers": [
+    "Strings",
+    "True or False",
+    "functions",
+    "if / else statements"
+  ]}
   
+]
 
-  
 
+
+document.getElementById('startTrivia').addEventListener('click', () => {
   
-</body>
-</html>
+})
+
+
